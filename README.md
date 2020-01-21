92ba134f39e462a29916cc7bc49a263965dfeb29
Joshua Byrd <phocks@gmail.com>
2020-01-21 11:49:45 +1000

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Audio Unmute
+
+A project generated from [aunty](https://github.com/abcnews/aunty)'s `basic` project template.
+
+## Authors
+
+- Joshua Byrd ([phocks@gmail.com](mailto:phocks@gmail.com))
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import App from './components/App';
+
+const PROJECT_NAME = 'audio-unmute';
+const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
+
+function init() {
+  root.appendChild(new App({ projectName: PROJECT_NAME }).el);
+}
+
+init();
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    root.removeChild(root.firstChild);
+
+    try {
+      init();
+    } catch (err) {
+      import('./components/ErrorBox').then(exports => {
+        const ErrorBox = exports.default;
+        root.appendChild(new ErrorBox({ error: err }).el);
+      });
+    }
+  });
+}
+
+if (process.env.NODE_ENV === 'development') {
+  console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
+}
